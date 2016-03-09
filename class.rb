352efb7ab60c4cf86627ab4d5a166b206883c7a6
Brandon Scott Brigance <brandon.brigance@gmail.com>
2016-03-09 00:14:39 +0000

    class.rb update

--- class.rb
+++ class.rb
@@ -31,3 +31,36 @@ all_students = [fred, sarah, jack]
 seniors(all_students).each do |student|
   puts student
 end
+
+#similiar version
+class Students
+  
+  attr_reader :first_name, :last_name, :grade
+  def initialize(first_name, last_name, grade)
+    @first_name = first_name
+    @last_name = last_name
+    @grade = grade
+  end
+  
+  def senior?
+    grade == 12
+  end
+  
+  def to_s
+    "#{first_name} #{last_name}"
+  end
+end
+  
+def seniors(students)
+  students.select { |student| student.senior? }
+end
+
+fred = Students.new("Fred", "James", 12)
+sarah = Students.new("Sarah", "Smith", 12)
+jack = Students.new("Jack", "Brown", 11)
+
+all_students = [fred, sarah, jack]
+
+seniors(all_students).each do |student|
+  puts student
+end
